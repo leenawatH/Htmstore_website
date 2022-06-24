6bc789dcb4053f77618ce099f08dd8f74e7925c5
leenawatH <leenawat.hon@student.mahidol.ac.th>
2022-06-24 16:16:11 +0700

    Nav create

---
+++ webpage/src/component/Nav.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../css/App.css';
+
+function Navbar(){
+    return <header className='Navbar'>
+                <div className='left-Nav'>
+                    <img src = {require('../img/logo/1x/logo.png')} width = '3%'/>
+                </div>
+                <div className='mid-Nav'>
+                    <a className='inNav'>Home</a>
+                    <a className='inNav'>Catagories</a>
+                    <a className='inNav'>About us</a>
+                </div>
+                <div className='right-Nav'>
+                    <img src = {require('../img/nav/face_logo.png')} width = '2%' className='imginright'/>
+                    <img src = {require('../img/nav/line_logo.png')} width = '2%' className='imginright'/>
+                    
+                    
+                </div>
+           </header>
+}
+
+export default Navbar;
---
+++ webpage/src/component/home-page/top-container.js
@@ -0,0 +1,7 @@
+import React from "react";
+import '../../css/App.css';
+function Container(){
+    return
+}
+
+export default Container;
