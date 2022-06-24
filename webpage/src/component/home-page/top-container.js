cb50439c5fddfcc42c61dcecc3cdf1741090d22f
leenawatH <leenawat.hon@student.mahidol.ac.th>
2022-06-24 17:02:00 +0700

    left top container

--- webpage/src/component/home-page/top-container.js
+++ webpage/src/component/home-page/top-container.js
@@ -1,7 +1,15 @@
 import React from "react";
 import '../../css/App.css';
+
+
 function Container(){
-    return
+    return <div className="top-container-left">
+                <a className="title-eng">Hor Tong Mong</a><br></br>
+                <a className="title">ฮ้อตงหมง</a>
+                <p className="description">ร้านขายอุปกรณ์ก่อสร้าง สีทาบ้าน </p>
+                <button type="button" class="btn btn-warning btn-lg">See Catagories</button>
+           </div>
+
 }
 
 export default Container;
--- webpage/src/index.js
+++ webpage/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Navbar from './component/Nav';
-
+import Top_left_container from './component/home-page/top-container';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div className='homepage'>
     <Navbar/>
+    <Top_left_container/>
   </div>
 );
 
